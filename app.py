831d715fd92a41d9f540a2aede689a09a609c86b
amohini099 <106774987+amohini099@users.noreply.github.com>
2022-07-15 15:49:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import pickle
 import pandas as pd
 from flask import Flask, request, render_template
 from flask_cors import cross_origin
-#import sklearn
 import pickle
 import pandas as pd
 # In[4]:
